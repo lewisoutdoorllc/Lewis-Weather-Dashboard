ac8923c8abec194d6313643d767c7fefa00a1b6d
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-01-28 19:41:56 -0500

    button working

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,2 +1,15 @@
-var city="";
+    function myFunction() {
+    var cityName = document.querySelector('#city-search').value;
+    
+    fetch(
+        'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=a4dc6b4797cfe0a360daceabbd77f8dc'
+    )
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
 
+   
+    });
+}
